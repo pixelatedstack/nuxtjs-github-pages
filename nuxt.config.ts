6ad9989ef8a0ad39b34016341653ef135052ffd2
Kennith Leung <kennith.leung@gmail.com>
2023-09-24 20:26:40 -0700

    build: site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/nuxtjs-github-pages/',
+    buildAssetsDir: '/assets/'
+  },
+  nitro: {
+    output: {
+      publicDir: 'docs'
+    }
+  },
 })
